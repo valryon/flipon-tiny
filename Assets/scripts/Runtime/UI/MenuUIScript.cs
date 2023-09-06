2c82ef3de48a09c46c7fcae7fc78842c15b005a3
Kacey Na <kacey.na01@gmail.com>
2023-09-06 02:44:54 -0500

    Mute button sets/resets audio levels, hint toggle has print statements, graphics dropdown changes project quality level settings

--- Assets/scripts/Runtime/UI/MenuUIScript.cs
+++ Assets/scripts/Runtime/UI/MenuUIScript.cs
@@ -4,46 +4,61 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using TMPro;
 
 public class MenuUIScript : MonoBehaviour
 {
     float loadingTimer = 0f;
     [SerializeField] GameObject bufferLogo;
 
-    public Toggle MuteVolumeToggle;
-    public Toggle NotifictationToggle;
-    public Toggle LivesToggle;
-    public Toggle DailyToggle;
-    public Toggle EventsToggle;
+    public Toggle muteVolumeToggle;
+    public Toggle notifictationToggle;
+    public Toggle livesToggle;
+    public Toggle dailyToggle;
+    public Toggle eventsToggle;
+    public Toggle hintsToggle;
 
     public AudioMixer mixer;
     public Slider musicSlider;
     public Slider soundSlider;
 
+    public TMP_Dropdown graphicsDropdown;
+
+    private float currentMusicVolume;
+    private float currentSoundVolume;
+
     private void Start()
     {
         // get mute toggle
-        // MuteVolumeToggle = GameObject.FindWithTag("Mute").GetComponent<Toggle>();
-        if (MuteVolumeToggle != null)
+        if (muteVolumeToggle != null)
         {
-            MuteVolumeToggle.isOn = false;
-            MuteVolumeToggle.onValueChanged.AddListener((bool isOn) => { Mute(MuteVolumeToggle.isOn); });
+            muteVolumeToggle.isOn = false;
+            muteVolumeToggle.onValueChanged.AddListener((bool isOn) => { Mute(muteVolumeToggle.isOn); });
         }
 
         // get notification toggles
-        // NotifictationToggle = GameObject.FindWithTag("Notifications").GetComponent<Toggle>();
-        if (NotifictationToggle != null)
+        if (notifictationToggle != null)
         {
-            NotifictationToggle.isOn = true;
-            NotifictationToggle.onValueChanged.AddListener((bool isOn) => { EnableNotifications(NotifictationToggle.isOn); });
+            notifictationToggle.isOn = true;
+            notifictationToggle.onValueChanged.AddListener((bool isOn) => { EnableNotifications(notifictationToggle.isOn); });
         }
 
-        // LivesToggle = GameObject.FindWithTag("Lives").GetComponent<Toggle>();
-        // DailyToggle = GameObject.FindWithTag("Daily").GetComponent<Toggle>();
-        // EventsToggle = GameObject.FindWithTag("Events").GetComponent<Toggle>();
+        // get hints toggle
+        if (hintsToggle != null)
+        {
+            hintsToggle.isOn = false;
+            hintsToggle.onValueChanged.AddListener((bool isOn) => { EnableHints(hintsToggle.isOn); });
+        }
+
+        // get quality dropdown
+
+        if (graphicsDropdown != null)
+        {
+            graphicsDropdown.onValueChanged.AddListener((int qualityIndex) => { SetQualityLevel(graphicsDropdown.value); }); 
+        }
     }
 
-
+    // load the game
     public void StartLoad(){
         StartCoroutine(AsyncLoadIntoGame());
     }
@@ -62,6 +77,7 @@ public class MenuUIScript : MonoBehaviour
         }
     }
 
+    // mute or set all volume based on toggle value
     void Mute(bool isOn)
     {
         if (isOn)
@@ -69,40 +85,68 @@ public class MenuUIScript : MonoBehaviour
             Debug.Log("Volume Muted");
             musicSlider.interactable = false;
             soundSlider.interactable = false;
+
+            mixer.SetFloat("MusicVolume", -80.0f);
+            mixer.SetFloat("SoundVolume", -80.0f);
         }
         else
         {
             Debug.Log("Volume Unmuted");
             musicSlider.interactable = true;
             soundSlider.interactable = true;
+
+            mixer.SetFloat("MusicVolume", currentMusicVolume);
+            mixer.SetFloat("SoundVolume", currentSoundVolume);
         }
     }
 
+    // enable notifications based on toggle, disable other notification settings
     void EnableNotifications(bool isOn)
     {
         if (isOn)
         {
             Debug.Log("Notifs enabled");
-            LivesToggle.interactable = true;
-            DailyToggle.interactable = true;
-            EventsToggle.interactable = true;
+            livesToggle.interactable = true;
+            dailyToggle.interactable = true;
+            eventsToggle.interactable = true;
 }
         else
         {
             Debug.Log("Notifs disabled");
-            LivesToggle.interactable = false;
-            DailyToggle.interactable = false;
-            EventsToggle.interactable = false;
+            livesToggle.interactable = false;
+            dailyToggle.interactable = false;
+            eventsToggle.interactable = false;
         }
     }
 
+    public void EnableHints(bool isOn)
+    {
+        if (isOn)
+        {
+            Debug.Log("Hints Enabled");
+        }
+        else
+        {
+            Debug.Log("Hints Disabled");
+        }
+    }
+
+    public void SetQualityLevel(int qualityIndex)
+    {
+        Debug.Log(qualityIndex);
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    // control music and sound levels with sliders
     public void SetMusicLevel(float sliderValue)
     {
         mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        currentMusicVolume = Mathf.Log10(sliderValue) * 20;
     }
 
     public void SetSoundLevel(float sliderValue)
     {
         mixer.SetFloat("SoundVolume", Mathf.Log10(sliderValue) * 20);
+        currentSoundVolume = Mathf.Log10(sliderValue) * 20;
     }
 }
