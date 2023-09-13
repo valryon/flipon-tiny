ee817d2a471a41d044a2e8ec4b3342d8431b92a6
Michael Routh <43078477+JohnMichaelRouth@users.noreply.github.com>
2023-09-13 11:38:14 -0500

    Changed the outlying files that used 4 space indentation

--- Assets/GoogleMobileAds/Editor/AndroidBuildPostProcessor.cs
+++ Assets/GoogleMobileAds/Editor/AndroidBuildPostProcessor.cs
@@ -8,32 +8,32 @@ using GoogleMobileAds.Editor;
 public static class AndroidBuildPostProcessor
 {
 
-    [PostProcessBuild]
-    public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
+  [PostProcessBuild]
+  public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
+  {
+    if (GoogleMobileAdsSettings.LoadInstance().GoogleMobileAdsAndroidAppId.Length == 0)
     {
-        if (GoogleMobileAdsSettings.LoadInstance().GoogleMobileAdsAndroidAppId.Length == 0)
-        {
-            NotifyBuildFailure(
-                "Android Google Mobile Ads app ID is empty. Please enter a valid app ID to run ads properly.");
-        }
+      NotifyBuildFailure(
+        "Android Google Mobile Ads app ID is empty. Please enter a valid app ID to run ads properly.");
     }
+  }
 
-    private static void NotifyBuildFailure(string message)
+  private static void NotifyBuildFailure(string message)
+  {
+    string prefix = "[GoogleMobileAds] ";
+
+    bool openSettings = EditorUtility.DisplayDialog(
+      "Google Mobile Ads", "Error: " + message, "Open Settings", "Close");
+    if (openSettings)
     {
-        string prefix = "[GoogleMobileAds] ";
-
-        bool openSettings = EditorUtility.DisplayDialog(
-            "Google Mobile Ads", "Error: " + message, "Open Settings", "Close");
-        if (openSettings)
-        {
-            GoogleMobileAdsSettingsEditor.OpenInspector();
-        }
+      GoogleMobileAdsSettingsEditor.OpenInspector();
+    }
 #if UNITY_2017_1_OR_NEWER
-        throw new BuildPlayerWindow.BuildMethodException(prefix + message);
+    throw new BuildPlayerWindow.BuildMethodException(prefix + message);
 #else
-        throw new OperationCanceledException(prefix + message);
+    throw new OperationCanceledException(prefix + message);
 #endif
-    }
+  }
 }
 
 #endif
--- Assets/GoogleMobileAds/Editor/EditorPathUtils.cs
+++ Assets/GoogleMobileAds/Editor/EditorPathUtils.cs
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//    http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,36 +23,36 @@ using UnityEngine;
  */
 public class EditorPathUtils : ScriptableObject
 {
-    /*
-     * Returns the asset path of EditorPathUtils.cs
-     */
-    private String GetFilePath()
-    {
-        return AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(this));
-    }
+  /*
+   * Returns the asset path of EditorPathUtils.cs
+   */
+  private String GetFilePath()
+  {
+    return AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(this));
+  }
 
-    /*
-     * Returns the asset directory path of EditorPathUtils.cs
-     */
-    public String GetDirectoryAssetPath()
-    {
-        return Path.GetDirectoryName(GetFilePath());
-    }
+  /*
+   * Returns the asset directory path of EditorPathUtils.cs
+   */
+  public String GetDirectoryAssetPath()
+  {
+    return Path.GetDirectoryName(GetFilePath());
+  }
 
-    /*
-     * Returns the parent asset directory path of EditorPathUtils.cs
-     */
-    public String GetParentDirectoryAssetPath()
-    {
-        return Path.GetDirectoryName(GetDirectoryAssetPath());
-    }
+  /*
+   * Returns the parent asset directory path of EditorPathUtils.cs
+   */
+  public String GetParentDirectoryAssetPath()
+  {
+    return Path.GetDirectoryName(GetDirectoryAssetPath());
+  }
 
-    /*
-     * Returns true if GMA import is done via unity package manager,
-     * false otherwise.
-     */
-    public bool IsPackageRootPath()
-    {
-        return GetFilePath().StartsWith("Packages");
-    }
+  /*
+   * Returns true if GMA import is done via unity package manager,
+   * false otherwise.
+   */
+  public bool IsPackageRootPath()
+  {
+    return GetFilePath().StartsWith("Packages");
+  }
 }
