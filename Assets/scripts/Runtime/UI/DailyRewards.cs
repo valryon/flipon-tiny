388fff6702e97091a729a996f9bc544f5e25dfa0
Kacey Na <kacey.na01@gmail.com>
2023-10-18 19:04:01 -0500

    fixing revert issues

--- Assets/scripts/Runtime/UI/DailyRewards.cs
+++ Assets/scripts/Runtime/UI/DailyRewards.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DailyRewards : MonoBehaviour
 {
@@ -17,16 +18,30 @@ public class DailyRewards : MonoBehaviour
     private string currentTimeInGame;
     // private string timeGameClosed;
 
+    // buttons
+    public Button[] rewardButtons = new Button[7];
+
 
     // Start is called before the first frame update
     void Start()
     {
         // DateTime start = DateTime.UtcNow; // use for first time playing ? ? ? ?
-        
+
         // check if user's first time opening game (need to track this in the title screen, store it/set boolean, check data here)
         if (firstTimePlaying)
         {
             currentDay = 1;
+            // set rewards
+            rewardButtons[0].interactable = true;
+            for (int i = 1; i < rewardButtons.Length; i++)
+            {
+                rewardButtons[i].interactable = false;
+            }
+
+            // no longer first time playing
+            // firstTimePlaying = false;
+            claimedReward = false;
+            isNewDay = false;
         }
         // if not first time opening, proceed as normal
         else
@@ -35,6 +50,12 @@ public class DailyRewards : MonoBehaviour
             timeGameOpened = DateTime.Now.ToString("HH:mm");
             // check if new day
             // if (timeGameOpened[:2])
+
+
+            // get time that the player last opened the game
+
+            // check if new day when game is reopened
+
         }
 
         // reset time is midnight
@@ -45,37 +66,66 @@ public class DailyRewards : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // time in game (military time 00:00 - 23:59)
         currentTimeInGame = DateTime.Now.ToString("HH");
         // print(currentTimeInGame);
 
         int currHour = int.Parse(currentTimeInGame);
 
         // if the hour has passed 00:00 and was not tracked that it is a new day, it is now a new day
-        if (currHour >= 0 && !isNewDay)
+        if (currHour >= 0 && isNewDay)
         {
-            isNewDay = true;
-
-            // new reward needs to unlock 
-            if (currentDay == 7)
+            if (firstTimePlaying)
             {
-                currentDay = 1;
+                firstTimePlaying = false;
             }
             else
             {
-                currentDay++;
+                isNewDay = false;
+
+                // new reward needs to unlock 
+                if (currentDay == 7)
+                {
+                    currentDay = 1;
+                }
+                else
+                {
+                    currentDay++;
+                }
+
+                UnlockReward(currentDay);
             }
-            
-            UnlockReward(currentDay);
+                
         }
         // otherwise, we are still in the same day 
     }
 
     // runs when user clicks on button
-    public void ClaimReward()
+    public void ClaimReward(int rewardAmount)
     {
         // check if user is able to claim reward
         // check if user already claimed reward
         // check if user is on the correct reward 
+
+        // current button
+        // Button currButton = rewardButtons[currentDay - 1];
+
+        if (!claimedReward)
+        {
+            // gain currency
+            print(rewardAmount);
+
+            print(currentDay);
+            // make button no longer interactable 
+            rewardButtons[currentDay - 1].interactable = false;
+
+            // change image of button
+            // currButton.GetComponent<Image>() = 
+
+            // make sure color is normal?
+
+            claimedReward = true;
+        }
     }
 
     public void UnlockReward(int currentDay)
@@ -90,4 +140,11 @@ public class DailyRewards : MonoBehaviour
 
         // set all buttons to uninteractable except first one
     }
+
+    /*
+    private void OnApplicationQuit()
+    {
+        firstTimePlaying = false; // ?
+    }
+    */
 }
